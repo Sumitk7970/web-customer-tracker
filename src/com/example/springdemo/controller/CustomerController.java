4f15632515c4315d02b3c36815d477073d4f5664
Sumit Kumar <kumarsumit8210@gmail.com>
2023-03-12 09:05:32 +0530

    Initial commit - List Customers

---
+++ src/com/example/springdemo/controller/CustomerController.java
@@ -0,0 +1,29 @@
+package com.example.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.springdemo.dao.CustomerDAO;
+import com.example.springdemo.entity.Customer;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@RequestMapping("/list")
+	public String listCustomers(Model theModel) {
+		
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		theModel.addAttribute("customers", customers);
+		
+		return "list-customers";
+	}
+}
